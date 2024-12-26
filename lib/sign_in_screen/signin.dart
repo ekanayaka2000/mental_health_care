719cc9303ea440c20350422209826108c31e0e41
Pramod Madhuranga <madhurangapramod2303@gmail.com>
2024-12-26 21:15:21 +0530

    fix signin and signup error

--- lib/sign_in_screen/signin.dart
+++ lib/sign_in_screen/signin.dart
@@ -12,10 +12,10 @@ class SignInPage extends StatefulWidget {
 class _SignInPageState extends State<SignInPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final FirebaseAuth _auth = FirebaseAuth.instance; // Firebase Auth instance
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  bool _agreeToTerms = false; // Checkbox state
-  bool _isLoading = false; // Loading state for button
+  bool _isPasswordVisible = false;
+  bool _isLoading = false;
 
   // Function to sign in the user
   Future<void> _signInUser() async {
@@ -24,19 +24,18 @@ class _SignInPageState extends State<SignInPage> {
     });
 
     try {
-      // Sign in the user with email and password
+      // Sign in with Firebase
       await _auth.signInWithEmailAndPassword(
         email: _emailController.text.trim(),
         password: _passwordController.text.trim(),
       );
 
-      // Navigate to the Home page if successful
+      // Navigate to HomePage on success
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (_) => HomePage()),
       );
     } on FirebaseAuthException catch (e) {
-      // Handle sign-in errors
       String message;
       if (e.code == 'user-not-found') {
         message = 'No user found for that email.';
@@ -65,174 +64,132 @@ class _SignInPageState extends State<SignInPage> {
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
-            // Navigate back to the signup page
             Navigator.pushReplacement(
                 context, MaterialPageRoute(builder: (_) => SignUpPage()));
           },
         ),
       ),
-      resizeToAvoidBottomInset: true, // Adjust layout for keyboard
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Center(
-                child: Column(
-                  children: [
-                    Image.asset(
-                      'assets/Logo1.png', // Replace with your logo path
-                      height: 100,
-                    ),
-                    SizedBox(height: 16),
-                  ],
-                ),
-              ),
-              SizedBox(height: 24),
-              Text(
-                "Welcome Back!",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 8),
-              Text(
-                "Sign in to continue your journey.",
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 32),
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  prefixIcon: Icon(Icons.email),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  prefixIcon: Icon(Icons.lock),
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.visibility),
-                    onPressed: () {
-                      // Handle password visibility toggle
-                    },
-                  ),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 8),
-              Row(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Center(
+              child: Column(
                 children: [
-                  Checkbox(
-                    value: _agreeToTerms,
-                    onChanged: (value) {
-                      setState(() {
-                        _agreeToTerms = value!; // Toggle checkbox state
-                      });
-                    },
+                  Image.asset(
+                    'assets/Logo1.png',
+                    height: 100,
                   ),
-                  Flexible(
-                    child: Text("I agree to MindLink terms & conditions"),
-                  ),
+                  SizedBox(height: 16),
                 ],
               ),
-              GestureDetector(
-                onTap: () {
-                  // Navigate to Forget Password page
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => ForgetPasswordPage()));
-                },
-                child: Text(
-                  "Forget Password?",
-                  style: TextStyle(color: Colors.red),
-                  textAlign: TextAlign.end,
-                ),
+            ),
+            SizedBox(height: 24),
+            Text(
+              "Welcome Back!",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 8),
+            Text(
+              "Sign in to continue your journey.",
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 32),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: "Email",
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 24),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF00BCD4), // Button color
-                  foregroundColor: Colors.white, // Text color
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _passwordController,
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+                labelText: "Password",
+                prefixIcon: Icon(Icons.lock),
+                suffixIcon: GestureDetector(
+                  onLongPress: () {
+                    setState(() {
+                      _isPasswordVisible = true;
+                    });
+                  },
+                  onLongPressUp: () {
+                    setState(() {
+                      _isPasswordVisible = false;
+                    });
+                  },
+                  child: Icon(
+                    _isPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                  ),
                 ),
-                onPressed: _isLoading
-                    ? null
-                    : () {
-                  if (_emailController.text.isNotEmpty &&
-                      _passwordController.text.isNotEmpty) {
-                    _signInUser(); // Attempt to sign in the user
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                          content: Text(
-                              "Please enter your email and password.")),
-                    );
-                  }
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 60.0, vertical: 16.0),
-                  child: _isLoading
-                      ? CircularProgressIndicator(color: Colors.white)
-                      : Text("Sign In"),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 8),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => ForgetPasswordPage()),
+                );
+              },
+              child: Text(
+                "Forget Password?",
+                style: TextStyle(color: Colors.red),
+                textAlign: TextAlign.end,
+              ),
+            ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF00BCD4),
+                foregroundColor: Colors.white,
+              ),
+              onPressed: _isLoading
+                  ? null
+                  : () {
+                if (_emailController.text.isNotEmpty &&
+                    _passwordController.text.isNotEmpty) {
+                  _signInUser();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                        content: Text(
+                            "Please enter your email and password.")),
+                  );
+                }
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 60.0, vertical: 16.0),
+                child: _isLoading
+                    ? CircularProgressIndicator(color: Colors.white)
+                    : Text("Sign In"),
+              ),
+            ),
+            SizedBox(height: 24),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => SignUpPage()));
+              },
+              child: Text(
+                "Don't have an account? Sign Up",
+                style: TextStyle(
+                  color: Color(0xFF00BCD4),
+                  decoration: TextDecoration.underline,
                 ),
+                textAlign: TextAlign.center,
               ),
-              SizedBox(height: 24),
-              Center(child: Text("Or continue with")),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.facebook),
-                    onPressed: () {
-                      // Handle Facebook sign-in
-                    },
-                  ),
-                  SizedBox(width: 16),
-                  GestureDetector(
-                    onTap: () {
-                      // Handle Google sign-in
-                    },
-                    child: Image.asset(
-                      'assets/google.png',
-                      height: 32,
-                      width: 32,
-                    ),
-                  ),
-                  SizedBox(width: 16),
-                  IconButton(
-                    icon: Icon(Icons.apple),
-                    onPressed: () {
-                      // Handle Apple sign-in
-                    },
-                  ),
-                ],
-              ),
-              SizedBox(height: 32),
-              GestureDetector(
-                onTap: () {
-                  // Navigate to SignUp page
-                  Navigator.pushReplacement(
-                      context, MaterialPageRoute(builder: (_) => SignUpPage()));
-                },
-                child: Text(
-                  "Don't have an account? Sign Up",
-                  style: TextStyle(
-                    color: Color(0xFF00BCD4),
-                    decoration: TextDecoration.underline,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
